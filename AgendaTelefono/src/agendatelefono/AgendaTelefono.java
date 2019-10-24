600d60f33cdc3deb31ca9a813febc2a19fefd8ea
alumno <alumno@aula1-02>
2019-10-24 14:35:26 +0200

    Sección de guardar contacto terminada

--- AgendaTelefono/src/agendatelefono/AgendaTelefono.java
+++ AgendaTelefono/src/agendatelefono/AgendaTelefono.java
@@ -27,11 +27,31 @@ public class AgendaTelefono {
 		}while(!((opcion >= 1) && (opcion <= 6)));
 		return opcion;
 	}
+	
+	public static int buscaHueco(String mContacto[][]) {
+		int pos = 0;
+		for (int i = 0; i<mContacto.length; i++) {
+			if (mContacto[i][0] == null) {
+				pos = i;
+				i = 50;
+			}
+		}
+		return pos;
+	}
+	
+	public static void guardaContacto(String mContacto[][]) {
+		int i = buscaHueco(mContacto);
+		Scanner leer = new Scanner(System.in);
+		System.out.println("Pon el nombre del contacto");
+		mContacto[i][0] = leer.nextLine(); 
+		System.out.println("Pon el teléfono del contacto");
+		mContacto[i][1] = leer.nextLine();
+	}
 
 	public static void main(String[] args) {
 		Scanner leer = new Scanner(System.in);
-		int opc = 0;
-		int mContacto[][] = new int[50][2];
+		int opc = 0, numCon = 0;
+		String mContacto[][] = new String[50][2];
 		
 		do {
 			opc = imprimeMenu();
@@ -40,7 +60,7 @@ public class AgendaTelefono {
 				System.out.println("Viendo contacto");
 				break;
 			case 2:
-				System.out.println("Guardando contacto");
+				guardaContacto(mContacto);
 				break;
 			case 3:
 				System.out.println("Eliminando contacto");
